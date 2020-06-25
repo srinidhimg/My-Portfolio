475dd795ed62e408a3e047a9ce4574d41ef6acc8
srinidhimg <srinidhimg1@gmail.com>
2020-06-25 18:49:56 +0530

    Deploying code

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '66707b2f9587a3423257a317deb51d70'
+# SECRET_KEY = '66707b2f9587a3423257a317deb51d70'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
