ca810c874c9da1b23c523f90022750666db5a562
srinidhimg <srinidhimg1@gmail.com>
2020-06-25 19:05:35 +0530

    Deployent 3

--- mywebsite/wsgi.py
+++ mywebsite/wsgi.py
@@ -12,12 +12,12 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+# from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mywebsite.settings")
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mywebsite.settings")
 
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+# application = get_wsgi_application()
+# application = DjangoWhiteNoise(application)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mywebsite.settings')
 
