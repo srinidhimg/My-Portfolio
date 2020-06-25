a5b16e46be2fd61cdb6de0fe45c1e7d176af3854
srinidhimg <srinidhimg1@gmail.com>
2020-06-25 19:02:16 +0530

    Deploymnet3

--- mywebsite/wsgi.py
+++ mywebsite/wsgi.py
@@ -14,7 +14,7 @@ from django.core.wsgi import get_wsgi_application
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bootcamp.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mywebsite.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
