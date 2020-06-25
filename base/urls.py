fdac0e23b10205e63dd0ce52695554cf318ee109
srinidhimg <srinidhimg1@gmail.com>
2020-06-25 23:23:05 +0530

    Deploy code 6

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path 
 from . import views
-
+from django.conf.urls.static import static
 
 urlpatterns =[
-path('', views.home)
+path('', views.home),
+    *static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
 ]
