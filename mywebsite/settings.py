c6c7245120f230a387226c94dcbefc627af00359
srinidhimg <srinidhimg1@gmail.com>
2020-06-25 19:32:58 +0530

    Deploy code 6

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/staticfiles/'
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 MEDIA_URL = '/Images/'
 
 STATICFILES_DIRS = [
