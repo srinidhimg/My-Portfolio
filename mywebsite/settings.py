33c08b4b40da96ca4775e3950d7855d1b81ab6e5
srinidhimg <srinidhimg1@gmail.com>
2020-06-25 20:15:51 +0530

    Deploy code 6

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT =os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT =os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 # STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 MEDIA_URL = '/Images/'
